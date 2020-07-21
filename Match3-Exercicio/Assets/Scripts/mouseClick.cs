7a6bf44dddc870a7c4fb5ec11d2741cd15f3adfe
annynaweb <sousa.annyc@gmail.com>
2020-07-21 05:08:37 -0300

    Seleciona e muda blocos de posicao, com audio effects

---
+++ Match3-Exercicio/Assets/Scripts/mouseClick.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseClick : MonoBehaviour
+{
+    private static mouseClick precedMudando = null; 
+    private bool dentro;
+    private bool arrastando;
+    private SpriteRenderer render;
+    private static Color pintou = new Color(.5f, .5f, .5f, 1.0f);
+    [SerializeField]private Rigidbody2D rb;
+     private void Start(){
+  
+       render = GetComponent<SpriteRenderer>();
+       
+    }
+   
+    private void Pegou(){
+     
+         dentro = true;
+        soundManager.PlaySound("Select");
+         render.color = pintou;
+         precedMudando = gameObject.GetComponent<mouseClick>();
+       
+    }
+
+    private void Soltou() {
+        render.color = Color.white;
+        precedMudando = null;
+        dentro = false;
+    }
+
+    void OnMouseDown(){
+       if(render.sprite == null || GridManager.instance.Mudando){
+            return;
+        }
+        if(dentro){
+            Soltou();
+        } else{
+            if (precedMudando == null){
+                Pegou();
+            } else{
+                MudaSprite(precedMudando.render);
+                precedMudando.Soltou();
+            }
+        }
+    }
+
+    void MudaSprite(SpriteRenderer render2){
+        if(render.sprite == render2.sprite){
+            return;
+        }
+
+        Sprite auxSprite = render2.sprite;
+        render2.sprite = render.sprite;
+        render.sprite = auxSprite;
+        soundManager.PlaySound("Swap");
+    }
+   
+
+   
+}
---
+++ Match3-Exercicio/Assets/Scripts/soundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public static AudioClip playerSelect, playerClear, playerSwap;
+    static AudioSource audioSource;
+       void Start()
+    {
+        playerClear = Resources.Load<AudioClip>("Clear");
+        playerSelect = Resources.Load<AudioClip>("Select");
+        playerSwap = Resources.Load<AudioClip>("Swap");
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+       void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip){
+        switch(clip){
+            case "Clear":
+                audioSource.PlayOneShot(playerClear);
+                break;
+            case "Select":
+                audioSource.PlayOneShot(playerSelect);
+                break;
+            case "Swap":
+                audioSource.PlayOneShot(playerSwap);
+                break;
+
+        }
+    }
+}
